f72abbf1850556d1673b3353c02452523da48eea
irwan8653 <tbs97377@gmail.com>
2025-06-21 23:10:34 +0700

    feat: Add Netlify to CORS allowlist

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ const allowedOrigins = [
   "http://localhost:5500", // Live Server
   "http://127.0.0.1:5500", // Live Server
   "http://localhost:3000", // npx serve
+  "https://wannabeen.netlify.app", //wannabeen
   // Tambahkan URL frontend produksi Anda di sini nanti, contoh: 'https://nama-website-anda.vercel.app'
 ];
 
